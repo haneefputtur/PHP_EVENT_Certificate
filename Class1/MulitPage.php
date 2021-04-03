2f3a6d92c4264028f3fed4ac64f7ccd59c66d264
Haneef Puttur <cdsputtur@gmail.com>
2021-04-03 19:34:16 +0400

    MUltiple Page PDF

---
+++ Class1/MulitPage.php
@@ -0,0 +1,33 @@
+<?php
+require('../fpdf/fpdf.php');
+
+//$name = text to be added, $x= x cordinate, $y = y coordinate, $a = alignment , $f= Font Name, $t = Bold / Italic, $s = Font Size, $r = Red, $g = Green Font color, $b = Blue Font Color
+function AddText($pdf, $text, $x, $y, $a, $f, $t, $s, $r, $g, $b) {
+$pdf->SetFont($f,$t,$s);	
+$pdf->SetXY($x,$y);
+$pdf->SetTextColor($r,$g,$b);
+$pdf->Cell(0,10,$text,0,0,$a);	
+}
+
+//Create A 4 Landscape page
+$pdf = new FPDF('L','mm','A4');
+function CreatePage($pdf,$name)
+{
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->SetCreator('Haneef Puttur');
+// Add background image for PDF
+$pdf->Image('../template.jpg',0,0,0);	
+
+//Add a Name to the certificate
+
+AddText($pdf,ucwords($name), 0,80, 'C', 'Helvetica','B',30,3,84,156);
+}
+CreatePage($pdf,'Mahammad Haneef');
+
+CreatePage($pdf,'Minaal');
+
+CreatePage($pdf,'Navneet');
+
+$pdf->Output();
+?>
